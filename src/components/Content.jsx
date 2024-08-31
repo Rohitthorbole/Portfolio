79871f0f84c879643e375545800aeb1cd6439355
Rohitthorbole <rohitthorbole4@gmail.com>
2024-08-31 06:50:28 +0530

    Update what i am currently doing

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,10 +5,17 @@ const Content = () => {
     return (
         <>
             <div className=' flex justify-between'>
+                {/* content-left */}
                 <div>
+
+                    {/* Name */}
+
                     <div className="flex justify-center font- pt-20 text-[40px] font-bold pl-[50px] max-w-[700px] font-deserta">
                         I'm Rohit. Web Developer and Aspiring Technologist
                     </div>
+
+                    {/* Brief Summary */}
+
                     <div className="pl-[50px] max-w-[700px] pt-10 text-[18px]">
                         I am Rohit Thorbole. I am Full Stack Web Developer who is eager to put my skills in development to use for the success. My knowledge on different technologies and programming languages ranges from C,C++, Python, PHP, Java, Javascript to managing databases and Linux environment.
                         <br />
@@ -16,62 +23,21 @@ const Content = () => {
                         I strongly believe in continuous learning and improving myself, so I try my best to learn in any situation possible, unfavorable or not.
                     </div>
 
-                    {/* Technologies */}
+                    {/* what i am doing */}
 
-                    <div className="pl-[50px] max-w-[700px] pt-10">
-                        <h1 className="text-[30px] font-semibold font-deserta">Technologies</h1>
-                        <ul class="text-[18px]">
-                            <li className="my-2 flex">
-                                <p className='text-blue-500'>React </p>
-                                <p> - UI Library</p> 
-                            </li>
-                            <li className="my-2 flex">
-                                <p className='text-blue-500'>Next.js </p>
-                                <p> - React Framework</p> 
-                            </li>
-                            <li className="my-2 flex">
-                                <p className='text-blue-500'>MongooDb </p>
-                                <p> - Database</p> 
-                            </li>
-                            <li className="my-2 flex">
-                                <p className='text-blue-500'>Tailwind CSS </p>
-                                <p> - Styling Library</p> 
-                            </li>
-                            <li className="my-2 flex">
-                                <p className='text-blue-500'>Python </p>
-                                <p> - Programming Language</p> 
-                            </li>
-                        </ul>
+                    <div className='pl-[50px] pt-[40px] max-w-[700px] text-[18px]'>
+                            <h1 className='text-[30px] font-semibold font-deserta'>What I'm Currently Working on?</h1>
+                            <p>I'm currently working with React and enhancing my skills in front-end. I'm also learning and working on back-end technologies. Currently, I am working on different Projects.</p>
                     </div>
 
-                    {/* Tools */}
+                    <div>
 
-                    <div className="pl-[50px] max-w-[700px] pt-10">
-                        <h1 className="text-[30px] font-semibold font-deserta">Tools</h1>
-                        <ul className="text-[18px]">
-                        <li className="my-2 flex">
-                                <p className='text-blue-500'>Visual Studio Code </p>
-                                <p> - Text Editor</p> 
-                            </li>
-                            <li className="my-2 flex">
-                                <p className='text-blue-500'>Pycharm </p>
-                                <p> - Python IDE</p> 
-                            </li>
-                            <li className="my-2 flex">
-                                <p className='text-blue-500'>Firefox </p>
-                                <p> - Web Browser</p> 
-                            </li>
-                            <li className="my-2 flex">
-                                <p className='text-blue-500'>Google Chrome </p>
-                                <p> - Web Browser</p> 
-                            </li>
-                            <li className="my-2 flex">
-                                <p className='text-blue-500'>Git Bash </p>
-                                <p> - Git Terminal</p> 
-                            </li>
-                        </ul>
                     </div>
+
                 </div>
+
+                {/* Content right */}
+
                 <div>
                     <div className='pt-[100px] pr-[100px]'><img src="./Media/rohit.png" alt="" className='max-h-[320px] max-w-[240px]rounded-lg hover:shadow-lg'/></div>
                 </div>
