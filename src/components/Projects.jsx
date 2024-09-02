799a55db2793ea66ba750ffa9740c3bdf0206e34
Rohitthorbole <rohitthorbole4@gmail.com>
2024-09-02 22:48:31 +0530

    my phone

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,7 +15,7 @@ const Projects = () => {
 
         {/* Description */}
 
-        <p className='pl-[50px] pt-4 text-[18px] xs:text-[16px] xs:max-w[95vw]'>
+        <p className='px-[50px] pt-4 text-[18px] xs:text-[16px] xs:max-w[95vw]'>
           I've worked on many of little projects over the years but these are the ones that I'm most proud of.  Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas on how it can be improved.
         </p>
 
